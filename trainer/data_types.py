7e13d14ae68688641ca7795e7441be5253f191df
jakseluz <labuzjak@gmail.com>
2025-12-14 00:45:19 +0100

    Implement model input data interface prototype with batches.
    Co-authored-by: kodziarz <gregor47@poczta.fm>

---
+++ trainer/data_types.py
@@ -0,0 +1,184 @@
+from typing import Iterator
+from dataclasses import dataclass
+import numpy as np
+import tensorflow as tf
+import random
+import math
+
+Sequence = tf.keras.utils.Sequence
+import numpy as np
+
+from grid_manager import Grid, GridManager
+from typing import Any
+
+InputGrid = np.ndarray[(Any, Any, 3), np.float64]
+"""Like normal Grid, but with bools indicating, if it should be changed (the 0-th coordinate of the thrid dimension). If it is False, then the IS_STREET bool is treated as zero."""
+
+OutputGrid = np.ndarray[(Any, Any, 3), np.float64]
+"""Like normal grid, but next to IS_STREET and ALTITUDE, it contains also IS_RESIDUAL value."""
+
+
+@dataclass
+class BatchSequence(Sequence):
+    """Keras Sequence that yields a batch of cuts from possibly different files.
+    Using Sequence improves performance by enabling the possibility of multi-threading and pre-fetching of data."""
+
+    def __init__(self, files: list[str], number_of_batches: int, batch_size: int, cut_sizes: list[tuple[int, int]]):
+        self._files = files
+        self._number_of_batches = number_of_batches
+        self._batch_size = batch_size
+        # Temporarily: one batch item -> random cut size from the given list
+        # OTHER POSSIBILITY: one batch -> one cut size
+        self._iterators = [
+            CutIterator(cut_sizes=cut_sizes, sequence=self) for i_file in range(batch_size)
+        ]  # pre-create iterators
+        # I was thinking about making each iterator correspond to a specific part (segments) of each file
+        # OR about reading firstly -> random parts of one file
+
+    def __len__(self):
+        """Get number of batches per epoch."""
+        return self._number_of_batches
+
+    def __getitem__(self) -> list[list[tuple[tuple[int, int], GridManager]]]:
+        """Generate one batch of data == multiple files turned to cuts (a list of batch items, each being a list of cuts).
+        Returns: batch - list of batch items (item == file), each being a list of cuts (start point and cut grid)."""
+        batch = []
+        for iterator in self._iterators:
+            batch_item = []
+            for _ in range(self._batch_size):
+                batch_item.append(next(iterator))
+            batch.append(batch_item)
+
+        return batch
+
+    def on_epoch_end(self):
+        # Optional method e.g. to shuffle data at the end of each epoch
+        pass
+
+
+@dataclass
+class CutIterator:
+    """Iterator that yields random cuts from a random file in a BatchSequence."""
+
+    def __init__(self, cut_sizes: list[tuple[int, int]], sequence: BatchSequence):
+        self._file_path = random.choice(list(sequence._files.keys()))
+        sequence._files[self._file_path] += 1  # increment count of uses
+        self._cut_sizes = cut_sizes
+
+        self._grid_manager = GridManager(self._file_path)  # load grid manager
+        self._grid_metadata = self._grid_manager.get_metadata()
+        self._grid_rows, self._grid_cols = self._grid_metadata.rows_number, self._grid_metadata.columns_number
+        self._segment_rows, self._segment_cols = self._grid_metadata.segment_h, self._grid_metadata.segment_w
+
+        self._max_x = self._grid_rows - min(cut[0] for cut in self._cut_sizes)  # max starting x for cut
+        self._max_y = self._grid_cols - min(cut[1] for cut in self._cut_sizes)  # max starting y for cut
+        self._already_used = list()  # list of already used starting points for cuts with their sizes
+
+        self._index = 0
+
+    def __iter__(self) -> Iterator[tuple[tuple[int, int], Grid], None, None]:
+        return self
+
+    def __next__(self) -> tuple[tuple[int, int], GridManager]:
+        """Get the next random cut (part of a batch item) from the file (a batch item == multiple cuts)."""
+        for _ in range(self._max_x * self._max_y):
+            # Choose random cut size
+            cut_size = self._cut_sizes[self._index % len(self._cut_sizes)]
+            if cut_size[0] > self._grid_rows or cut_size[1] > self._grid_cols:
+                cut_size = (self._grid_rows, self._grid_cols)
+
+            self._max_x = self._grid_rows - cut_size[0]  # max starting x for cut
+            self._max_y = self._grid_cols - cut_size[1]  # max starting y for cut
+
+            # Choose random starting point
+            start_x = random.randint(0, self._max_x)
+            start_y = random.randint(0, self._max_y)
+
+            self._already_used.append(((start_x, start_y), cut_size))
+
+            # Calculate end point
+            end_x = start_x + cut_size[0]
+            end_y = start_y + cut_size[1]
+
+            # Determine which segments to read
+            which_segment_start_x = start_x // self._segment_rows
+            which_segment_start_y = start_y // self._segment_cols
+            which_segment_end_x = end_x // self._segment_rows
+            which_segment_end_y = end_y // self._segment_cols
+
+            cut_x = np.array([])  # Grid()
+            # Go by segments and merge them into one bigger cut - first vertically, then horizontally.
+            for indx_x in range(which_segment_start_x, which_segment_end_x + 1):
+                for indx_y in range(which_segment_start_y, which_segment_end_y + 1):
+                    # TODO - memory issue
+                    segment_y = self._grid_manager.read_segment(indx_x, indx_y)
+                    # Merge segment_y into cut_y vertically.
+                    if indx_y == which_segment_start_y:
+                        cut_y = segment_y
+                    else:
+                        cut_y = np.vstack((cut_y, segment_y))
+
+                # Merge cut_y into cut_x horizontally.
+                if indx_x == which_segment_start_x:
+                    cut_x = cut_y
+                else:
+                    cut_x = np.hstack((cut_x, cut_y))
+
+            cut_x = cut_x.resize(1, (cut_size[0], cut_size[1], cut_x.shape[2] + 1))  # is modifiable
+            cut_segment_rows = math.ceil(
+                cut_size[0] / self._grid_metadata.segment_h
+            )  # number of segments in cut vertically
+            cut_segment_columns = math.ceil(
+                cut_size[1] / self._grid_metadata.segment_w
+            )  # number of segments in cut horizontally
+            cut_grid = GridManager[InputGrid](
+                f"{self._file_path}_cut_{start_x}_{start_y}_{cut_size[0]}_{cut_size[1]}.dat",
+                cut_size[0],
+                cut_size[1],
+                0,
+                0,
+                None,
+                self._grid_metadata.segment_h,
+                self._grid_metadata.segment_w,
+                "./tmp/batches/batch_sequences/cuts/",
+            )
+            for segment_row in range(cut_segment_rows):
+                for segment_col in range(cut_segment_columns):
+                    cut_grid.write_segment(
+                        cut_x[
+                            segment_row
+                            * self._grid_metadata.segment_h : min(
+                                (segment_row + 1) * self._grid_metadata.segment_h, cut_size[0]
+                            ),
+                            segment_col
+                            * self._grid_metadata.segment_w : min(
+                                (segment_col + 1) * self._grid_metadata.segment_w, cut_size[1]
+                            ),
+                        ],
+                        segment_row,
+                        segment_col,
+                    )
+
+            self._index += 1
+            return ((start_x, start_y), cut_grid)
+
+        raise StopIteration
+
+    def count_used_start_points(self) -> int:
+        """Count the number of used starting points for cuts (with duplicates)."""
+        return len(self._already_used)
+
+    def count_unique_start_points(self) -> int:
+        """Count the number of unique used starting points for cuts (without duplicates)."""
+        return len(set(self._already_used[:, 0]))
+
+    def calculate_area_covered(self) -> float:
+        """Calculate the area covered by the cuts made so far as a fraction of the total file area."""
+        total_area = self._grid_rows * self._grid_cols
+        all_points = [0 for _ in range(self._grid_rows)] * [0 for _ in range(self._grid_cols)]
+        for (start_x, start_y), cut_size in set(self._already_used):
+            for i in range(cut_size[0]):
+                for j in range(cut_size[1]):
+                    all_points[start_x + i][start_y + j] = 1
+        covered_area = sum([sum(row) for row in all_points])
+        return covered_area / total_area
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,14 +1,8 @@
-from typing import Generator, Iterator
+from typing import Iterator
 from dataclasses import dataclass
-import numpy as np
-import tensorflow as tf
-import random
 
-Sequence = tf.keras.utils.Sequence
-import numpy as np
-
-from grid_manager import Grid, GridManager
 from trainer.model import Model
+import data_types
 
 
 @dataclass
@@ -25,136 +19,12 @@ class Trainer:
         """Perform training on model.
         Args:
             fits_count (int): Number of fits to perform."""
+
+        batchSeq = data_types.BatchSequence(
+            files=list(self._files.keys()),
+            number_of_batches=1,
+            batch_size=1,
+            cut_size=[(32, 32), (64, 64), (128, 128)],
+        )
         # TODO
-
-    def _cut_random_file(self, cut_size: tuple[int, int]) -> Iterator[tuple[tuple[int, int], Grid], None, None]:
-        """Cut segments from a random file.
-        Args:
-            cut_size (tuple[int, int]): Size of the cut to make (rows, columns) and then to pass to the model."""
-        # TODO ?
-
-
-@dataclass
-class BatchSequence(Sequence):
-    """Keras Sequence that yields a batch of cuts from possibly different files.
-    Using Sequence improves performance by enabling the possibility of multi-threading and pre-fetching of data."""
-
-    def __init__(self, trainer: Trainer, number_of_batches: int, batch_size: int, cut_size: list[tuple[int, int]]):
-        self._files = trainer._files
-        self._number_of_batches = number_of_batches
-        self._batch_size = batch_size
-        # Temporarily: one batch item -> random cut size from the given list
-        # OTHER POSSIBILITY: one batch -> one cut size
-        self._iterators = [
-            CutIterator(cut_size=cut_size, sequence=self) for i_file in range(batch_size)
-        ]  # pre-create iterators
-        # I was thinking about making each iterator correspond to a specific part (segments) of each file
-        # OR about reading firstly -> random parts of one file
-
-    def __len__(self):
-        """Get number of batches per epoch."""
-        return self._number_of_batches
-
-    def __getitem__(self) -> list[list[tuple[tuple[int, int], Grid]]]:
-        """Generate one batch of data == multiple files turned to cuts (a list of batch items, each being a list of cuts)."""
-        batch = []
-        for iterator in self._iterators:
-            batch_item = []
-            for _ in range(self._batch_size):
-                batch_item.append(next(iterator))
-            batch.append(batch_item)
-        return batch
-
-    def on_epoch_end(self):
-        # Optional method e.g. to shuffle data at the end of each epoch
-        pass
-
-
-@dataclass
-class CutIterator:
-    """Iterator that yields random cuts from a random file in a BatchSequence."""
-
-    def __init__(self, cut_size: list[tuple[int, int]], sequence: BatchSequence):
-        self._file_path = random.choice(list(sequence._files.keys()))
-        sequence._files[self._file_path] += 1  # increment count of uses
-        self._cut_size = cut_size
-
-        self._grid_manager = GridManager(self._file_path)  # load grid manager
-        self._grid_metadata = self._grid_manager.get_metadata()
-        self._grid_rows, self._grid_cols = self._grid_metadata.rows_number, self._grid_metadata.columns_number
-        self._segment_rows, self._segment_cols = self._grid_metadata.segment_h, self._grid_metadata.segment_w
-
-        self._max_x = self._grid_rows - min(cut[0] for cut in self._cut_size)  # max starting x for cut
-        self._max_y = self._grid_cols - min(cut[1] for cut in self._cut_size)  # max starting y for cut
-        self._already_used = list()  # list of already used starting points for cuts with their sizes
-
-    def __iter__(self) -> Iterator[tuple[tuple[int, int], Grid], None, None]:
-        return self
-
-    def __next__(self) -> tuple[tuple[int, int], Grid]:
-        """Get the next random cut (part of a batch item) from the file (a batch item == multiple cuts)."""
-        for _ in range(self._max_x * self._max_y):
-            # Choose random cut size
-            cut_size = random.choice(self._cut_size)
-            if cut_size[0] > self._grid_rows or cut_size[1] > self._grid_cols:
-                # TODO
-                cut_size = (self._grid_rows, self._grid_cols)
-
-            self._max_x = self._grid_rows - cut_size[0]  # max starting x for cut
-            self._max_y = self._grid_cols - cut_size[1]  # max starting y for cut
-
-            # Choose random starting point
-            start_x = random.randint(0, self._max_x)
-            start_y = random.randint(0, self._max_y)
-
-            self._already_used.append(((start_x, start_y), cut_size))
-
-            # Calculate end point
-            end_x = start_x + cut_size[0]
-            end_y = start_y + cut_size[1]
-
-            # Determine which segments to read
-            which_segment_start_x = start_x // self._segment_rows
-            which_segment_start_y = start_y // self._segment_cols
-            which_segment_end_x = end_x // self._segment_rows
-            which_segment_end_y = end_y // self._segment_cols
-
-            cut_x = Grid()
-            # Go by segments and merge them into one bigger cut - first vertically, then horizontally.
-            for indx_x in range(which_segment_start_x, which_segment_end_x + 1):
-                for indx_y in range(which_segment_start_y, which_segment_end_y + 1):
-                    segment_y = self._grid_manager.read_segment(indx_x, indx_y)
-                    # Merge segment_y into cut_y vertically.
-                    if indx_y == which_segment_start_y:
-                        cut_y = segment_y
-                    else:
-                        cut_y = np.vstack((cut_y, segment_y))
-
-                # Merge cut_y into cut_x horizontally.
-                if indx_x == which_segment_start_x:
-                    cut_x = cut_y
-                else:
-                    cut_x = np.hstack((cut_x, cut_y))
-
-            return ((start_x, start_y), cut_x)
-
-        raise StopIteration
-
-    def count_used_start_points(self) -> int:
-        """Count the number of used starting points for cuts (with duplicates)."""
-        return len(self._already_used)
-
-    def count_unique_start_points(self) -> int:
-        """Count the number of unique used starting points for cuts (without duplicates)."""
-        return len(set(self._already_used[:, 0]))
-
-    def calculate_area_covered(self) -> float:
-        """Calculate the area covered by the cuts made so far as a fraction of the total file area."""
-        total_area = self._grid_rows * self._grid_cols
-        all_points = [0 for _ in range(self._grid_rows)] * [0 for _ in range(self._grid_cols)]
-        for (start_x, start_y), cut_size in set(self._already_used):
-            for i in range(cut_size[0]):
-                for j in range(cut_size[1]):
-                    all_points[start_x + i][start_y + j] = 1
-        covered_area = sum([sum(row) for row in all_points])
-        return covered_area / total_area
+        # model.fit(batchSeq, epochs=fits_count)
