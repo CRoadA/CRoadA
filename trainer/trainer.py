9b0e26f362fd1a780d87571aa49d45c7f497ba17
jakseluz <labuzjak@gmail.com>
2026-02-10 13:05:57 +0100

    Correct main and comments

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,22 +1,25 @@
 from dataclasses import dataclass
 
+from grid_manager import Grid, GridManager
 from trainer.clipping_model import ClippingModel
 
 
 @dataclass
 class Trainer:
-    def __init__(self, model: ClippingModel, files: list[str]):
+    def __init__(self, model: ClippingModel, files: list[GridManager[Grid]]):
         """Initialize trainer with model and files.
         Args:
             model: Model to be trained.
-            files (list[str]): A set of files to learn the model on."""
+            files (list[GridManager[Grid]]): A set of files to learn the model on."""
         self._model = model
         self._files = files
 
     def random_fit_from_files(self, epochs: int = 100, steps_per_epoch=1000, batch_size: int = 32):
         """Perform training on model.
         Args:
-            fits_count (int): Number of fits to perform."""
+            epochs (int): Number of epochs to train.
+            steps_per_epoch (int): Number of steps per epoch.
+            batch_size (int): Size of each training batch."""
         self._model.fit(
             train_files=self._files,
             val_files=self._files,
